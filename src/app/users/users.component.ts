c1e9da04069de7b090c6d4166293efb1f8d0c758
Mosiah Azuaje <mosiahazuaje2010@gmail.com>
2020-07-12 21:33:58 -0500

    Actualizaciones de codigo mejoras

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -166,22 +166,25 @@ export class UsersComponent implements OnInit {
     const time = Date.now().toString();
     let mail = '<div style="position:relative;min-width:320px;">'+
     '<div class="clearfix borderbox" style="z-index:1;min-height:535px;background-image:none;border-width:0px;border-color:#000000;background-color:transparent;padding-bottom:62px;width:100%;max-width:800px;margin-left:auto;margin-right:auto;">'+
-    '<div class="clearfix colelem" style="z-index:2;background-color:#000000;position:relative;margin-right:-10000px;width:100%;">'+
+    '<div class="clearfix colelem" style="z-index:2;background-color:#39B54A;position:relative;margin-right:-10000px;width:100%;">'+
     '<div class="grpelem"><div></div></div>'+
     '<div class="clearfix grpelem" style="z-index:4;min-height:17px;background-color:transparent;line-height:18px;color:#FFFFFF;font-size:15px;font-family:roboto, sans-serif;font-weight:300;position:relative;margin-right:-10000px;width:19.25%;left:7.13%;" data-muse-temp-textContainer-sizePolicy="true" data-muse-temp-textContainer-pinning="true">'+
-    '<p>¡Hola '+name+'!</p></div></div>'+
+    '<p style="text-align:center; color:#ffffff">¡Hola '+name+'!</p></div></div>'+
     '<div class="colelem" style="z-index:3;height:133px;opacity:1;filter:alpha(opacity=100);margin-top:91px;position:relative;width:18.13%;margin-left:40.13%; background-size: contain;">'+
     '<img src="http://159.203.82.152/assets/logo.png" alt=""></div>'+
     '<div class="clearfix colelem" style="z-index:13;min-height:17px;background-color:transparent;line-height:22px;color:#000000;font-size:18px;font-family:roboto, sans-serif;font-weight:700;margin-top:78px;position:relative;width:46.13%;margin-left:26.13%;">'+
-    '<p>¡FELICIDADES, HAS SIDO ACEPTADO!</p></div>'+
+    '<p>Felicidades, has aprobado nuestro proceso de verificaci&oacute;n Satisfactoriamente, a partir de este momento quedas activo en Wintowin.</p>'+
+    '<p>Estamos conformando una comunidad que permita contar con una oferta y demanda suficiente de movilidad grauita, segura y comoda, por esto el servicio de la aplicaci&oacute;n empezar&aacute; efectivamente desde el <strong>1ro de Agosto de 2020</strong>.</p>'+
+    '<p>Agradecemos tu paciencia, recuerda mantener instalada las App para particpar en los sorteos que llevar&aacute; a cabo WIntowin entre los usuarios activos, te invitamos a consultar nuestra pag&iacute;na web www.wintowin.com.co y nuestras redes sociales, encontrar&aacute;s todos los videos tutoriales de funcionalidad de la aplicaci&oacute;n.</p>'+
+    '<h6>Preparate para el cambio</h6></div>'+
     '<div class="clearfix colelem" style="z-index:17;min-height:17px;background-color:transparent;line-height:22px;color:#000000;font-size:18px;font-family:roboto, sans-serif;font-weight:700;margin-top:77px;position:relative;width:46.13%;margin-left:26.13%;" data-muse-temp-textContainer-sizePolicy="true" data-muse-temp-textContainer-pinning="true">'+
     '<p>Ya puedes utilizar nuestra plataforma desde este momento… ¡Te esperamos!.</p></div>'+
     '<div class="clearfix colelem" style="background-color:#000000; height:48px; z-index:8;margin-top:111px;width:100%;">'+
     '<div class="grpelem" style="display:inline;float:left; z-index:8;background-color:#000000;position:relative;margin-right:-10000px;width:100%;">'+
     '<div class="fluid_height_spacer"></div></div><div class="clearfix grpelem" style="height: 17px; display:inline;float:left;z-index:9;min-height:17px;background-color:transparent;line-height:18px;color:#FFFFFF;font-size:15px;font-family:roboto, sans-serif;font-weight:300;position:relative;margin-right:-10000px;width:10%;left:43%; ">'+
-    '<p>WIN to WIN</p></div></div>'+
+    '<p style="text-align:center; color:#ffffff">WIN to WIN</p></div></div>'+
     '<div class="clearfix colelem" style="z-index:21;min-height:17px;background-color:transparent;line-height:14px;color:#000000;font-size:12px;font-family:roboto, sans-serif;font-weight:300;margin-top:9px;position:relative;width:45.88%;margin-left:28.38%;" data-muse-temp-textContainer-sizePolicy="true" data-muse-temp-textContainer-pinning="true">'+
-    '<p>¿Tienes dudas? Envíanos un mensaje a soporte@wintowin.co</p></div><div class="verticalspacer" data-offset-top="600" data-content-above-spacer="599" data-content-below-spacer="61" data-sizePolicy="fixed" data-pintopage="page_fixedLeft"></div>'
+    '<p>¿Tienes dudas? Env&iacute;anos un mensaje a soporte@wintowin.com.co</p></div><div class="verticalspacer" data-offset-top="600" data-content-above-spacer="599" data-content-below-spacer="61" data-sizePolicy="fixed" data-pintopage="page_fixedLeft"></div>'
     +'</div></div>';
     this.corek.socket.emit('confsmtp',{'key':'confsmtp','token':8,'event':time + 'conf'});
     this.corek.socket.on(time + 'conf', (data, key)=>{
